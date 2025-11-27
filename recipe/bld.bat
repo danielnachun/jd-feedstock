c4afab1fb67c0131ee2696653f967cb6493236d0
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-26 21:04:33 -0800

    use rattler

--- recipe/bld.bat
+++
@@ -1,4 +0,0 @@
-@echo on
-
-go build -buildmode=pie -trimpath -ldflags "-w -X main.revision=conda-forge" -v -o %LIBRARY_PREFIX%\bin\jd.exe
-go-licenses save . --save_path .\library_licenses --ignore github.com/josephburnett/jd
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-go build -buildmode=pie -trimpath -ldflags "-w -s -X main.revision=conda-forge" -v -o $PREFIX/bin/jd
+go build -ldflags "-w -s -X main.revision=conda-forge" -v -o $PREFIX/bin/jd
 go-licenses save . --save_path ./library_licenses --ignore github.com/josephburnett/jd
